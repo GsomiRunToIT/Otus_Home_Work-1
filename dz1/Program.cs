71049fae3fc1d4ce543342ad46db1e6d1b8a3f14
ru.51@mail.ru <ru.51@mail.ru>
2023-05-30 22:50:39 +0300

    1

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,17 +1,11 @@
-﻿
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.Metrics;
-using System.Reflection;
-using System.Text;
-
-namespace dz1
+﻿namespace dz1Otus_Home_Work_1
 {
     internal class Program
     {
         static void Main()
         {
             Console.WriteLine("Введите число от 1 до 6");
+            int tableSize;
             int numberValue;
             bool textNotNumber;
             string text;
@@ -23,144 +17,140 @@ namespace dz1
                     Console.WriteLine("Неккоректное чило,введите число от 1 до 6 ");
             }
             while (textNotNumber == false);
-            int tableSize;
             int inputLength;
-            bool textNotVoid;
+            bool textNotVoid=false;
+            text = Console.ReadLine();
+            inputLength = text.Length;
+            tableSize = (numberValue - 1) * 2 + inputLength + 2;
+            for (int i = 0; i < 4; i++)
+            {
+                Border(tableSize);
+                switch (i)
+                {
+                    case 0:
+                        FirstCell(inputLength, textNotVoid, numberValue, tableSize, text);
+                        break;
+                    case 1:
+                        CenterCell(tableSize, numberValue);
+                        break;
+                    case 2:
+                        LastCell(tableSize, numberValue); 
+                        break;
+                }
+            }
+        }
+        static void FirstCell(int inputLength, bool textNotVoid, int numberValue, int tableSize, string text)
+        {
             do
             {
-                text = Console.ReadLine();
-                inputLength = text.Length;
                 textNotVoid = !String.IsNullOrWhiteSpace(text) && text.Length < 41;
                 if (!textNotVoid)
                     Console.WriteLine("Туть пусто");
             }
             while (textNotVoid == false);
-            Console.WriteLine("CONGRATILACION");
-            Console.Clear();
-            tableSize = (numberValue - 1) * 2 + inputLength + 2;
-            string border = new string('+', tableSize);
-            Console.WriteLine(border);
             for (int counter1 = 1; counter1 < numberValue; counter1++)
                 Console.WriteLine("+".PadRight(tableSize - 1, ' ') + ('+'));
             Console.WriteLine("+".PadRight(numberValue, ' ') + text + "+".PadLeft(numberValue, ' '));
             for (int counter2 = 1; counter2 < numberValue; counter2++)
                 Console.WriteLine("+".PadRight(tableSize - 1, ' ') + ('+'));
+            string border = new string('+', tableSize);
+        }
+        static void Border(int tableSize)
+        {
+            string border = new string('+', tableSize);
             Console.WriteLine(border);
-
+        }
+        static void CenterCell(int tableSize, int numberValue)
+        {
             int height = (numberValue - 1) * 2 + 1;
             for (int plus = 0; plus < height; plus++)
             {
                 string kto_kuda = null;
-                for (int spase = 0; spase < tableSize - 1; spase++)
+                for (int space = 0; space < tableSize - 1; space++)
                 {
-                    kto_kuda += ((plus + spase) % 2 == 0) ? "+" : " ";
+                    kto_kuda += ((plus + space) % 2 == 0) ? "+" : " ";
                     int firtPosition = 0;
-                    //int lastPosition = tableSize-1 ;
                     char replacement = '+';
                     kto_kuda = (kto_kuda.Remove(firtPosition, 1).Insert(firtPosition, replacement.ToString()));
-                    //kto_kuda = (kto_kuda.Remove(lastPosition, 1).Insert(lastPosition, replacement.ToString()));
                 }
-                Console.WriteLine(kto_kuda + "+");//костыль
+                Console.WriteLine(kto_kuda + "+");
             }
-            Console.WriteLine(border);
-
+        }
+        static void LastCell(int tableSize, int numberValue)
+        {
             string cross = null;
             string centerSpaсe;
             string left_rightSpace;
-
-            int quentityCenterSpase = tableSize - 4;
-            int quentityLeftRightSpace = 0;
-
+            int quantityCenterSpaсe = tableSize - 4;
+            int quantityLeftRightSpace = 0;
 
             for (int counter = 0; counter < tableSize - 1; counter++)
             {
-                string lenght_centerSpaсe = new string(' ', quentityCenterSpase);
-                string lenghtLeftRightSpace = new string(' ', quentityLeftRightSpace);
-                //чёт
+                string lenghtCenterSpaсe = new string(' ', quantityCenterSpaсe);
+                string lenghtLeftRightSpace = new string(' ', quantityLeftRightSpace);
                 if (tableSize % 2 == 0 && counter < (tableSize - 2) / 2 - 1)
                 {
                     for (int counter1 = (tableSize - 2) / 2; counter1 >= 0; counter1--)
                     {
-                        cross = "+" + lenghtLeftRightSpace + "+" + lenght_centerSpaсe + "+" + lenghtLeftRightSpace + "+";
+                        cross = "+" + lenghtLeftRightSpace + "+" + lenghtCenterSpaсe + "+" + lenghtLeftRightSpace + "+";
                         Console.WriteLine(cross);
-                        quentityCenterSpase -= 2;
-                        quentityLeftRightSpace += 1;
+                        quantityCenterSpaсe -= 2;
+                        quantityLeftRightSpace += 1;
                         break;
                     }
                 }
-                else if (tableSize % 2 == 0 && quentityCenterSpase == 0 && counter < (tableSize - 2) / 2)
+                else if (tableSize % 2 == 0 && quantityCenterSpaсe == 0 && counter < (tableSize - 2) / 2)
                 {
-                    cross = "+" + lenghtLeftRightSpace + "+" + lenght_centerSpaсe + "+" + lenghtLeftRightSpace + "+";
+                    cross = "+" + lenghtLeftRightSpace + "+" + lenghtCenterSpaсe + "+" + lenghtLeftRightSpace + "+";
                     Console.WriteLine(cross);
-                }
-                
+                } 
                 if (tableSize % 2 == 0 && counter > (tableSize - 2) / 2)
                 {
                     for (int counter2 = (tableSize - 2) / 2; counter2 >= 0; counter2--)
                     {
-                        cross = "+" + lenghtLeftRightSpace + "+" + lenght_centerSpaсe + "+" + lenghtLeftRightSpace + "+";
+                        cross = "+" + lenghtLeftRightSpace + "+" + lenghtCenterSpaсe + "+" + lenghtLeftRightSpace + "+";
                         Console.WriteLine(cross);
-                        quentityCenterSpase += 2;
-                        quentityLeftRightSpace -= 1;
+                        quantityCenterSpaсe += 2;
+                        quantityLeftRightSpace -= 1;
                         break;
                     }
                 }
-                // нечёт -----------------------------------------------------------------------------------------
-
-
                 if (tableSize % 2 == 1 && counter < (tableSize - 3) / 2 - 1)
                 {
                     for (int counter3 = (tableSize - 3) / 2; counter3 >= 0; counter3--)
                     {
-                        cross = "+" + lenghtLeftRightSpace + "+" + lenght_centerSpaсe + "+" + lenghtLeftRightSpace + "+";
+                        cross = "+" + lenghtLeftRightSpace + "+" + lenghtCenterSpaсe + "+" + lenghtLeftRightSpace + "+";
                         Console.WriteLine(cross);
-                        quentityCenterSpase -= 2;
-                        quentityLeftRightSpace += 1;
+                        quantityCenterSpaсe -= 2;
+                        quantityLeftRightSpace += 1;
                         break;
                     }
                 }
-                
-
-                else if (tableSize % 2 == 1 && quentityCenterSpase == 1 && counter == (tableSize - 3) / 2 )
+                else if (tableSize % 2 == 1 && counter == (tableSize - 3) / 2 + 1)
                 {
-                   
-                    cross = "+" + lenghtLeftRightSpace + "+" + lenghtLeftRightSpace + "+";
-                    Console.WriteLine(cross);
-                    
-
-                }
-                else if (tableSize % 2 == 1 && counter == (tableSize - 3) / 2+1 )
-                {
-                   // quentityLeftRightSpace = (tableSize - 3) / 2;
-                    cross = "+" + lenghtLeftRightSpace + "+" + lenght_centerSpaсe + "+" + lenghtLeftRightSpace + "+";
+                    cross = "+ " + lenghtLeftRightSpace + "+" + lenghtLeftRightSpace + " +";
                     Console.WriteLine(cross);
                 }
-                //else if (tableSize % 2 == 1 && counter1 == (tableSize  - 3 ) / 2 + 1 )
-                //{
-                //  quentityLeftRightSpace  +=1 ;
-                //}
-
-                if (tableSize % 2 == 1 && counter > (tableSize - 3) / 2+1 )
+                else if (tableSize % 2 == 1 && counter == (tableSize - 3) / 2)
                 {
-                    
+                    cross = "+" + lenghtLeftRightSpace + "+" + lenghtCenterSpaсe + "+" + lenghtLeftRightSpace + "+";
+                    Console.WriteLine(cross);
+                }
+                if (tableSize % 2 == 1 && counter > (tableSize - 3) / 2 + 1)
+                {
                     for (int counter4 = (tableSize - 3) / 2; counter4 >= 0; counter4--)
                     {
-                        cross = "+" + lenghtLeftRightSpace + "+" + lenght_centerSpaсe + "+" + lenghtLeftRightSpace + "+";
+                        cross = "+" + lenghtLeftRightSpace + "+" + lenghtCenterSpaсe + "+" + lenghtLeftRightSpace + "+";
                         Console.WriteLine(cross);
-                        quentityCenterSpase += 2;
-                        quentityLeftRightSpace -= 1;
+                        quantityCenterSpaсe += 2;
+                        quantityLeftRightSpace -= 1;
                         break;
                     }
                 }
-               
-               
-
-
             }
         }
     }
 }
-
 
 
 
